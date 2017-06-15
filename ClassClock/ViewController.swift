797a8ec4f4e62049c4b93e0938a97d13509d25c6
Ericka Allen <erickaallen@macbook-pro>
2017-06-14 20:49:51 -0400

    Initial Commit

---
+++ ClassClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassClock
+//
+//  Created by Ericka Allen on 6/14/17.
+//  Copyright © 2017 Ericka Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
